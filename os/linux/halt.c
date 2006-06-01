8006f7fe8abfeb678563a8277132c3868e59f869
Nico Schottelius <nico@hydrogenium.schottelius.org>
2006-06-02 00:14:32 +0200

    Changed description for halt/reboot/poweroff, removed unecessary headers

--- os/linux/halt.c
+++ os/linux/halt.c
@@ -4,14 +4,8 @@
  * reboot
  */
 
-#include <sys/mount.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <sys/reboot.h>
 #include <unistd.h>
-#include <signal.h>
-#include <stdio.h>
-#include <time.h>
 
 #include "cinit.h"
 
