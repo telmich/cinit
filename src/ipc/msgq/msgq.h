5d11a80813def692515f7ce92343d8f79ae01b90
Nico Schottelius <nico@hydrogenium.schottelius.org>
2006-07-28 22:22:45 +0200

    Add header for msgq functions

---
+++ src/ipc/msgq/msgq.h
@@ -0,0 +1,12 @@
+/* 
+ * (c) 2006 Nico Schottelius (nico-linux-cinit //@\\ schottelius.org)
+ * part of cinit
+ */
+
+#ifndef CINIT_IPC_HEADER
+#define CINIT_IPC_HEADER
+
+int mq_server;                   /* to the server           */
+int mq_client;                   /* to the clients          */
+
+#endif
