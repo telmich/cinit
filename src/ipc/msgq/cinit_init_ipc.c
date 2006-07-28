0447100e032c6c3ec2cb3aa0c90c89777ab3deb4
Nico Schottelius <nico@hydrogenium.schottelius.org>
2006-07-28 22:07:58 +0200

    NEW: src/ipc/msgq/cinit_init_ipc.c

---
+++ src/ipc/msgq/cinit_init_ipc.c
@@ -0,0 +1,23 @@
+/* 
+ * (c) 2006 Nico Schottelius (nico-linux-cinit //@\\ schottelius.org)
+ * part of cinit
+ */
+
+#include <sys/ipc.h>             /* ftok */
+#include <sys/msg.h>             /* msgget */
+
+
+PERROR?!
+
+int cinit_ipc_init(void)
+{
+   key_t k_tmp;
+
+   k_tmp = ftok(MSGQ_PATHNAME,MSGQ_TO_SERVER);
+
+   if(k_tmp == -1) return 0;
+   
+   mq_server = msgget(k_tmp,0600 | IPC_CREAT);
+   if(mq_server == -1) return 0;
+
+}
