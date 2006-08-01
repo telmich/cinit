30a8c497f41b6a36f3795275ca4b9f65a1e66145
Nico Schottelius <nico@hydrogenium.schottelius.org>
2006-08-01 18:01:58 +0200

    Add prototypes for OS-specific functions

---
+++ src/headers/os.h
@@ -0,0 +1,19 @@
+/* 
+ * (c) 2006 Nico Schottelius (nico-linux-cinit //@\\ schottelius.org)
+ * part of cinit
+ *
+ * os-specific functions
+ */
+
+#ifndef CINIT_IPC_HEADER
+#define CINIT_IPC_HEADER
+
+/*****************************************************************************
+ * Functions
+ */
+
+void cinit_halt(void);
+void cinit_poweroff(void);
+void cinit_reboot(void);
+
+#endif
