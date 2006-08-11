a2537d7b2f8eb18a7ec671a91045a2112adab979
Nico Schottelius <nico@hydrogenium.schottelius.org>
2006-08-11 21:32:25 +0200

    Only set handlers for signals we actually use

--- src/core/set_signals.c
+++ src/core/set_signals.c
@@ -17,28 +17,14 @@ void set_signals(int action)
    struct sigaction sa;
 
    if(action == ACT_SERV) {
-      sa.sa_handler=SIG_IGN;
-   } else {
-      sa.sa_handler=SIG_DFL;
-   }
-   sigaction(SIGINT,&sa,NULL);      /* ignore ctr+c == ctr+alt+del      */
-   sigaction(SIGPIPE,&sa,NULL);     /* what todo when pipe/fifo closed  */
-   sigaction(SIGTTIN,&sa,NULL);     /* just ignore                      */
-   sigaction(SIGTTOU,&sa,NULL);     /* just ignore                      */
-   sigaction(SIGSTOP,&sa,NULL);     /* just ignore                      */
-   sigaction(SIGQUIT,&sa,NULL);     /* just ignore                      */
-
-   /* cleaning dead processes - only process 1 */
-   if(action == ACT_SERV) {
       sa.sa_handler=sig_child;
    } 
    sigaction(SIGCHLD,&sa,NULL);     /* what todo when a child exited    */
 
-   /* signal handlers to do special things with: reboot */
    if(action == ACT_SERV) {
       sa.sa_handler=do_reboot;
    }
-   sigaction(SIGHUP,&sa,NULL);      /* reboot */
-   sigaction(SIGTERM,&sa,NULL);     /* poweroff */
-   sigaction(SIGUSR1,&sa,NULL);     /* halt */
+   sigaction(SIGHUP,&sa,NULL);      /* reboot                           */
+   sigaction(SIGTERM,&sa,NULL);     /* poweroff                         */
+   sigaction(SIGUSR1,&sa,NULL);     /* halt                             */
 }
