bf1a90c4b792bf027fffc9fa90efc786a543081a
Nico Schottelius <nico@hydrogenium.schottelius.org>
2006-08-11 20:04:05 +0200

    New question type in documentation

--- src/headers/comm.h
+++ src/headers/comm.h
@@ -15,16 +15,17 @@
  * maximum number of commands: 2^8 = 256
  */
 enum commands {
-   CMD_START_SVC=1,  /* the client wants US to start a service    */
-   CMD_STOP_SVC,     /* the clients wants us to stop the service  */
-   CMD_STOP_SVC_REC, /* stop service and all depending services   */
-   CMD_CHG_STAT,     /* the client reports a status change        */
-   CMD_RESCUE,       /* we should start the rescue mode           */
-   CMD_HALT,         /* halt the system                           */
-   CMD_REBOOT,       /* reboot the system                         */
-   CMD_POWEROFF,     /* poweroff the system                       */
-   CMD_WBOOT,        /* shutdown and restart everything           */
-   CMD_INFO          /* send information about that service       */
+   CMD_START_SVC=1,     /* the client wants US to start a service    */
+   CMD_START_SVC_ONLY,  /* start this service without dependencies   */
+   CMD_STOP_SVC,        /* the clients wants us to stop the service  */
+   CMD_STOP_SVC_ONLY,   /* we should stop _only_ this service        */
+   CMD_CHG_STAT,        /* the client reports a status change        */
+   CMD_RESCUE,          /* we should start the rescue mode           */
+   CMD_HALT,            /* halt the system                           */
+   CMD_REBOOT,          /* reboot the system                         */
+   CMD_POWEROFF,        /* poweroff the system                       */
+   CMD_WBOOT,           /* shutdown and restart everything           */
+   CMD_INFO             /* send information about that service       */
 };
 
 /***********************************************************************
@@ -50,21 +51,28 @@ enum svc_status {
 
 /***********************************************************************
  * messages from the client
+ * Prefix: qst_ (question)
  */
 struct msg_client {
-   pid_t pid;           /* pid of the telling client        */
-   char cmd;            /* which cmd is issued to us        */
-   char status;         /* status of the service            */
-   char cmdstatus       /* both in one byte?                */
-   char name[PATH_MAX]; /* name of the service              */
+   pid_t pid;                 /* pid of the telling client              */
+   char cmd;                  /* which cmd is issued to us              */
+   char status;               /* status of the service                  */
+   char cmdstatus             /* both in one byte?                      */
+   char svc[PATH_MAX];        /* name of the service                    */
+   char svc_orig[PATH_MAX];   /* the service, which wants to start us   */
 };
 
 /***********************************************************************
  * messages from the server
+ * Prefix: asw_ (answer)
  */
-struct msg_server {
 
-
+/***********************************************************************
+ * return short status
+ */
+struct asw_sstatus {
+   long  mtype;
+   char  status;
 };
 
 #endif
