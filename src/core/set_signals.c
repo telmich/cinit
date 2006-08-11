c9383aa0aa352495f388f6c6cc67e2877560767f
Nico Schottelius <nico@hydrogenium.schottelius.org>
2006-08-11 21:30:51 +0200

    First signal handler cleanup

--- src/core/set_signals.c
+++ src/core/set_signals.c
@@ -1,8 +1,12 @@
-/* 
- * (c) 2005 Nico Schottelius (nico-linux at schottelius.org)
- * cinit.c
- * part of cLinux/cinit
+/***********************************************************************
+ *
+ *    2006-2006 Nico Schottelius (nico-linux-cinit //@\\ schottelius.org)
+ *
+ *    part of cLinux/cinit
+ *
+ *    Define signal handlers
  */
+
 
 #include <signal.h>
 #include <stdio.h>
@@ -17,28 +21,24 @@ void set_signals(int action)
    } else {
       sa.sa_handler=SIG_DFL;
    }
-   sigaction(SIGINT,&sa,NULL);      /* ignore ctr+c and ctr+alt+del     */
+   sigaction(SIGINT,&sa,NULL);      /* ignore ctr+c == ctr+alt+del      */
    sigaction(SIGPIPE,&sa,NULL);     /* what todo when pipe/fifo closed  */
-   sigaction(SIGTTIN,&sa,NULL);
-   sigaction(SIGTTOU,&sa,NULL);
-   sigaction(SIGSTOP,&sa,NULL);
-
-   sigaction(SIGQUIT,&sa,NULL);     /* warm reboot??? */
+   sigaction(SIGTTIN,&sa,NULL);     /* just ignore                      */
+   sigaction(SIGTTOU,&sa,NULL);     /* just ignore                      */
+   sigaction(SIGSTOP,&sa,NULL);     /* just ignore                      */
+   sigaction(SIGQUIT,&sa,NULL);     /* just ignore                      */
 
    /* cleaning dead processes - only process 1 */
    if(action == ACT_SERV) {
       sa.sa_handler=sig_child;
    } 
-   /* server and client need sig_chld - really??? FIXME in exec_svc then! */
    sigaction(SIGCHLD,&sa,NULL);     /* what todo when a child exited    */
 
    /* signal handlers to do special things with: reboot */
    if(action == ACT_SERV) {
       sa.sa_handler=do_reboot;
    }
-   sigaction(SIGUSR1,&sa,NULL);     /* halt */
-   sigaction(SIGUSR2,&sa,NULL);     /* Rescue */
-   sigaction(SIGTERM,&sa,NULL);     /* poweroff */
    sigaction(SIGHUP,&sa,NULL);      /* reboot */
-   sigaction(SIGCONT,&sa,NULL);     /* update */
+   sigaction(SIGTERM,&sa,NULL);     /* poweroff */
+   sigaction(SIGUSR1,&sa,NULL);     /* halt */
 }
