1edb92cac720a78905548c1444aec27a07834fdc
Nico Schottelius <nico@hydrogenium.schottelius.org>
2006-10-18 08:13:37 +0200

    Template for service tree generation

---
+++ src/core/gen_svc_tree.c
@@ -0,0 +1,15 @@
+/***********************************************************************
+ *
+ *    2005-2006 Nico Schottelius (nico-linux-cinit at schottelius.org)
+ *
+ *    part of cLinux/cinit
+ *
+ *    Pre calculate the service tree
+ */
+
+#include "cinit.h"
+#include <unistd.h>
+
+int gen_svc_tree(char *start)
+{
+}
