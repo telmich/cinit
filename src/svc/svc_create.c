80a50da585bc91c2196c76a0299b8feb3d49b1bf
Nico Schottelius <nico@hydrogenium.schottelius.org>
2006-10-18 18:52:19 +0200

    Add svc_create

---
+++ src/svc/svc_create.c
@@ -0,0 +1,24 @@
+/***********************************************************************
+ *
+ *    2005-2006 Nico Schottelius (nico-linux-cinit at schottelius.org)
+ *
+ *    part of cLinux/cinit
+ *
+ *    Check whether service is existent
+ */
+
+#include "cinit.h"
+#include <stdio.h>      /* NULL */
+
+int svc_create(char *svc)
+{
+   /* status is currently -1 -> unset */
+   /* FIXME: do NOT use list_insert, as it does not
+    * check whether the service already exists!
+    *
+    * Is that really an issue? We are most likely only called
+    * from gen_svc_tree, which checks before...
+    */
+
+   return list_insert(svc,-1);
+}
