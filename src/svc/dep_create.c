a3c92c35e80506fdfb603daa776079145b4bd330
Nico Schottelius <nico@hydrogenium.schottelius.org>
2006-11-20 09:15:00 +0100

    [NEW] dep_create
    Abstract creating a dependency to dep_create

---
+++ src/svc/dep_create.c
@@ -0,0 +1,26 @@
+/***********************************************************************
+ * 
+ *    2006 Nico Schottelius (nico-cinit at schottelius.org)
+ * 
+ *    part of cLinux/cinit
+ * 
+ *    Create dependency
+ */
+
+#include <stdio.h>         /* NULL              */
+#include <stdlib.h>        /* malloc()          */
+#include "cinit.h"
+#include "svc.h"
+
+/* search for an entry by path, reverse: from end to the beginning */
+struct dep *dep_create(struct listitem *svc)
+{
+   struct dep *entry;
+   
+   entry = malloc(sizeof(struct dep));
+   if(!entry) return NULL;
+   entry->svc = svc;
+
+   return entry;
+
+}
