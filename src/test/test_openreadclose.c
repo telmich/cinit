6021ec58f97e9546a0408b2de59a56838637fb4a
Nico Schottelius <nico@hydrogenium.schottelius.org>
2006-11-23 08:53:23 +0100

    Test for openreadclose

---
+++ src/test/test_openreadclose.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int openreadclose(char *filename, char **where);
+
+int main()
+{
+   char *data;
+   char *file = "test_openreadclose.c";
+
+   openreadclose(file,&data);
+
+   printf("%s\n",data);
+
+   return 1;
+
+}
