8b53fa83f4da167ba882e604f3b06a271b83e6b9
Nico Schottelius <nico@wasserstoff.schottelius.org>
2006-11-29 23:09:59 +0100

    Add: svc_fail()

---
+++ src/svc/svc_fail.c
@@ -0,0 +1,20 @@
+/***********************************************************************
+ *
+ *    2006 Nico Schottelius (nico-cinit at schottelius.org)
+ *
+ *    part of cLinux/cinit
+ *
+ *    Mark the service as being failed
+ */
+
+#include "svc.h"
+
+void svc_fail(struct listitem *li)
+{
+   if(li->status & ST_SH_ONCE)
+      li->status = ST_ONCE_FAIL;
+   else {
+      /* FIXME: do something senseful, record time of dead? */
+      li->status = ST_RESPAWNING;
+   }
+}
