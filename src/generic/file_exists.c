b7cbbe7f8675b27762d8eaebea7fa7e56251ab7f
Nico Schottelius <nico@hydrogenium.schottelius.org>
2006-12-06 17:47:31 +0100

    [ADD] file_exists.c

---
+++ src/generic/file_exists.c
@@ -0,0 +1,33 @@
+/***********************************************************************
+ *
+ *    2006 Nico Schottelius (nico-cinit at schottelius.org)
+ *
+ *    part of cLinux/cinit
+ *
+ *    check whether file exists and is a file
+ */
+
+#include <sys/types.h>     /* FIXME: check headers against posix */
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <errno.h>         /* errno                   */
+
+int file_exists(char *filename)
+{
+   struct stat buf;
+   
+   if(stat(filename,&buf) == -1) {
+      if(errno == ENOENT) {
+         return FE_NOT;
+      } else {
+         return FE_ERR;
+      }
+   } else {
+      if(!S_ISREG(buf.st_mode)) {
+         return FE_OTHER;
+      }
+   }
+
+   return FE_FILE;
+}
