82ec1c005649e9a7fb27d21ff81a153393f93851
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-02-07 20:59:04 +0100

    Prepare do_reboot to do something senseful

--- src/core/do_reboot.c
+++ src/core/do_reboot.c
@@ -10,7 +10,6 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <signal.h>
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
@@ -19,6 +18,9 @@
 #include "cinit.h"
 #include "os.h"
 #include "messages.h"
+
+/* cleaned headers */
+#include <signal.h>              /* kill()            */
 
 /***********************************************************************
  * sig_reboot
@@ -32,16 +34,35 @@ void do_reboot(int signal)
    //char **cmd;
    //int   i;
    
-   /* shutdown all services: take care about the dependency tree */
+   /*
+    * New code:
+    * 0. close user ipc
+    *    - notify user!
+    *    - do not allow user interupt anymore
+    * 1. shutdown services
+    *    - notify user!
+    * 2. kill -TERM all processes
+    *    - notify user!
+    * 3. kill -KILL all processes
+    *    - notify user!
+    * 4. execute /etc/cinit/conf/{halt,reboot,poweroff}
+    *    - notify user!
+    */
+   
+   /* FIXME: pre-shutdown? not senseful, can be implemented outside. */
 
-   /* do not listen to client requests anymore
-    * FIXME: perhaps before shutdown? */
+   /* do not listen to client requests anymore */
    cinit_ipc_destroy();
 
+   /* shutdown all services: take care about the dependency tree */
+   cinit_svc_shutdown();
+
    /* now: all services are down, let's kill all other processes */
-   if( kill(-1,SIGTERM) == -1) {
+   if(kill(-1,SIGTERM) == -1) {
       print_errno(MSG_TERMKILL);
    }
+
+   /* FIXME: read sleep value from conf/term_kill_sleep */
 
    /* FIXME make SLEEP_KILL an optional configuration statement */
    ts.tv_sec   = SLEEP_KILL; /* defined in conf/sleep_kill */
