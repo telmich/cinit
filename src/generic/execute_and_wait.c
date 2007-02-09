21aa672536e7af8c57e0eab035d41b1b46333045
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-02-09 19:25:46 +0100

    Execute and wait

---
+++ src/generic/execute_and_wait.c
@@ -0,0 +1,37 @@
+/***********************************************************************
+ *
+ *    2007 Nico Schottelius (nico-cinit at schottelius.org)
+ *
+ *    part of cLinux/cinit
+ *
+ *    Execute something, but fork() before
+ *
+ */
+
+#include <sys/types.h>     /* pid_t       */
+#include <sys/wait.h>      /* waitpid     */
+#include <unistd.h>        /* fork        */
+#include "messages.h"      /* MSG_*       */
+#include "cinit.h"         /* print_errno, execute_sth */
+
+void execute_and_wait(char *svc)
+{
+   pid_t pid;
+   int status;
+
+   /* fork */
+   pid = fork();
+   
+   if(pid == -1) {
+      print_errno(MSG_ERR_FORK);
+      return;
+   }
+   
+   /* exec / child */
+   if(pid == 0) {
+      execute_sth(svc); /* exits itself */
+   }
+
+   /* wait / parent */
+   waitpid(pid,&status,0);
+}
