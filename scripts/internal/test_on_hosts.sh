1678acc72ba55d9ca19662dae30b05dafe4753e4
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-02-17 20:25:23 +0100

    Add new test host, prefix lines with hostname

--- scripts/internal/test_on_hosts.sh
+++ scripts/internal/test_on_hosts.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # yah
 
-hosts="penrose.bsdprojects.net bot.unixkiste.org"
+hosts="penrose.bsdprojects.net bot.unixkiste.org suckz.eu"
 
 set -e
 
 for host in $hosts; do
    echo "$1 on $host"
-   "$1" "$host"
+   "$1" "$host" | sed "s/^/${host}: /"
    echo "==> Finished $host"
 done
