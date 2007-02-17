3f1408c336ac40f4205563ca41ae45cd631d16f5
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-02-17 18:13:42 +0100

    compile and run on testhosts

---
+++ scripts/internal/compile+run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# yah - yet another hack
+
+host="$1"
+
+hier=$(dirname $0)
+
+$hier/compile_test.sh "$1"
+
+if [ $? -ne 0 ]; then
+   echo "aborting, did not compile on $1"
+   exit 1
+fi
+
+ssh "${host}" "./cinit/src/cinit"
