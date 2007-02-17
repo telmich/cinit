764c9fb53e0719ec4b8175684d0bf0c78280be47
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-02-17 16:25:25 +0100

    Create a test configuration

---
+++ scripts/internal/create_test_config.sh
@@ -0,0 +1,18 @@
+hier=$(dirname $0)
+
+set -e
+${hier}../cinit.install.standard.dirs
+cat > /etc/cinit/svc/init/on << eof
+#!/bin/sh
+echo test1
+sleep 4
+echo test2 - \$@
+eof
+chmod 0755 /etc/cinit/svc/init/on
+
+cat > /etc/cinit/svc/init/on.params << eof
+es
+scheint
+zu
+gehen
+eof
