fa66b4999aa134bfcf7746a3f282b438ff2d9a43
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-02-17 20:59:27 +0100

    Add note about glibc.
    Glibc is fun!
    
    Have a look at http://sourceware.org/bugzilla/show_bug.cgi?id=4067

--- src/core/set_signals.c
+++ src/core/set_signals.c
@@ -9,30 +9,33 @@
 
 
 #include <signal.h>        /* sigaction, sigemtpyset  */
-#include <stdio.h>         /* NULL           */
-#include "cinit.h"         /* defines        */
+#include <stdio.h>         /* NULL                    */
+#include "cinit.h"         /* defines                 */
 
 void set_signals(int action)
 {
    struct sigaction sa;
 
+   /* If you want to have fun with glibc, comment out the three lines that
+    * initialise sa.sa_flags and cinit will segfault when sig_child
+    * exits. "Works" on glibc 2.3-2.5 ;-)
+    */
    if(action == ACT_SERV) {
       sa.sa_handler  = sig_child;
       sa.sa_flags    = SA_NOCLDSTOP; 
    } else {
-      sa.sa_handler=SIG_DFL;
-      sa.sa_flags    = 0;
+      sa.sa_handler  =  SIG_DFL;
+      sa.sa_flags    =  0;
    }
    sigemptyset(&sa.sa_mask);
 
    sigaction(SIGCHLD,&sa,NULL);     /* what todo when a child exited    */
 
-   /* reset flags */
-   sa.sa_flags    = 0;
+   sa.sa_flags = 0;                 /* reset flags */
    if(action == ACT_SERV) {
-      sa.sa_handler=do_reboot;
+      sa.sa_handler = do_reboot;
    }
-   sigaction(SIGHUP,&sa,NULL);      /* reboot                           */
-   sigaction(SIGTERM,&sa,NULL);     /* poweroff                         */
-   sigaction(SIGUSR1,&sa,NULL);     /* halt                             */
+   sigaction(SIGHUP,  &sa, NULL);   /* reboot    */
+   sigaction(SIGTERM, &sa, NULL);   /* poweroff  */
+   sigaction(SIGUSR1, &sa, NULL);   /* halt      */
 }
