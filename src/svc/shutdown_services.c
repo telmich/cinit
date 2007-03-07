73a9c37dc71a8db5f71e1da6ebcd4281d0c27c2c
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-03-07 22:54:00 +0100

    Add template for shutdown_services

---
+++ src/svc/shutdown_services.c
@@ -0,0 +1,14 @@
+/***********************************************************************
+ *
+ *    2005-2007 Nico Schottelius (nico-cinit at schottelius.org)
+ *
+ *    part of cLinux/cinit
+ *
+ *    Shutdown all services
+ */
+
+void shutdown_services(void)
+{
+   
+   return;
+}
