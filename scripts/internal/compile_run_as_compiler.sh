34a750b75571d72b9d024cf0ef70c27bc50dae52
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-03-07 23:46:57 +0100

    Fix test-script for compile+run locally

--- scripts/internal/compile_run_as_compiler.sh
+++ scripts/internal/compile_run_as_compiler.sh
@@ -2,6 +2,8 @@ what=$(dirname $0)/../../
 what_real=$(cd "$what" && pwd -P)
 dest=~compiler/cinit-test
 
-rsync -av "${what_real}/" "$dest"
-chown -R compiler "$dest"
-su - compiler -c "cd $dest && make -C src all && ./src/cinit"
+set -e
+sudo rsync -av "${what_real}/" "$dest"
+sudo chown -R compiler "$dest"
+sudo -u compiler "${dest}/scripts/internal/compile_local.sh"
+sudo -u compiler "${dest}/src/cinit"
