a3870b1fc279e7a4dff94775e658d894a92f1420
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-03-08 11:36:53 +0100

    Need hostname

--- scripts/internal/compile+run.sh
+++ scripts/internal/compile+run.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 # yah - yet another hack
 
+if [ ! "$1" ]; then
+   echo "HOSTNAME"
+   exit 23
+fi
+
 host="$1"
 
 hier=$(dirname $0)
 
-$hier/compile_test.sh "$1"
+$hier/compile_test.sh "$host"
 
 if [ $? -ne 0 ]; then
    echo "aborting, did not compile on $1"
