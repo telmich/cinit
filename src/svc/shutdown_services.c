0e542cb1f91dc0cda3210bc5e2c630273bdb0814
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-04-13 13:56:41 +0200

    Add testing with uml

--- src/svc/shutdown_services.c
+++ src/svc/shutdown_services.c
@@ -1,6 +1,6 @@
 /***********************************************************************
  *
- *    2005-2007 Nico Schottelius (nico-cinit at schottelius.org)
+ *    2007 Nico Schottelius (nico-cinit at schottelius.org)
  *
  *    part of cLinux/cinit
  *
@@ -9,6 +9,15 @@
 
 void shutdown_services(void)
 {
-   
+   /* ipc must be stopped before! */
+
+   /* stop services in the correct order:
+    * - begin at a random process, and walk the tree up to the wanted_by and needed_by
+    * - switch to next service, as soon as this tree is finished
+    */
+
+   /* OR: create list of services that noone wants/needs
+    * - fillup this list everytime a service si not wanted/needed by others anymore
+    */
    return;
 }
