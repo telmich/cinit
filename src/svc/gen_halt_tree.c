4afa387f39f23ff7fff80b90183a457347d6144a
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-04-23 20:54:26 +0200

    Create a halt tree, similar to gen_svc_tree

---
+++ src/svc/gen_halt_tree.c
@@ -0,0 +1,40 @@
+/***********************************************************************
+ *
+ *    2006 Nico Schottelius (nico-cinit at schottelius.org)
+ *
+ *    part of cLinux/cinit
+ *
+ *    Pre calculate the service tree
+ */
+
+#include <stdlib.h>
+#include "cinit.h"
+#include "svc.h"
+
+struct listitem *gen_svc_tree(char *svc)
+{
+   struct listitem   *li;
+   struct dep        *deps;
+
+   /* only do something if the service is not already known */
+   if((li=list_search(svc)))           return li;
+
+   /* create a template, so other instances won't try to recreate us */
+   if(!(li=svc_create(svc)))           return NULL;
+
+   if(!check_add_deps(li,DEP_NEEDS))   return NULL;
+   if(!check_add_deps(li,DEP_WANTS))   return NULL;
+
+   /* no dependencies? then you are a start service */
+   if(!li->wants && !li->needs) {
+      deps = dep_create(li);
+      if(!deps) return NULL;
+      dep_entry_add(&svc_init,deps);
+
+      /* Mark it as being in the startup list, so it does not
+       * get added again in a dep_needs_wants_add call */
+      li->status |= ST_IN_LIST;
+   }
+
+   return li;
+}
