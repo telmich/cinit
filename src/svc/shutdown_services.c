23054b00452ef69cc87dd33c9c0f93b46d3bbd2f
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-04-23 20:54:02 +0200

    Add comments about how shutdown_services will work

--- src/svc/shutdown_services.c
+++ src/svc/shutdown_services.c
@@ -4,20 +4,25 @@
  *
  *    part of cLinux/cinit
  *
- *    Shutdown all services
+ *    Shutdown all services: This is more or less the negative
+ *    version of 'tree_exec()'
  */
 
-void shutdown_services(void)
+void shutdown_services(struct listitem *svc)
 {
-   /* ipc must be stopped before! */
-
    /* stop services in the correct order:
     * - begin at a random process, and walk the tree up to the wanted_by and needed_by
     * - switch to next service, as soon as this tree is finished
     */
 
-   /* OR: create list of services that noone wants/needs
-    * - fillup this list everytime a service si not wanted/needed by others anymore
-    */
+   while(we_are_wanted_or_needed) {
+      shutdown_services(next_wants_or_needs_us);
+   }
+
+   shutdown_services(svc) 
+   
+   /* begin at svc, iterate until a service without dependencies is found,
+    * continue stopping through wanted-by / needed-by / wants / needs */
+
    return;
 }
