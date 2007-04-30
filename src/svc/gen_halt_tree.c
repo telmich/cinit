2ff94cbc04342bc5113ea1bce54026a993f85434
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-04-30 23:30:46 +0200

    Introduce pseudo-code

--- src/svc/gen_halt_tree.c
+++ src/svc/gen_halt_tree.c
@@ -1,40 +1,43 @@
 /***********************************************************************
  *
- *    2006 Nico Schottelius (nico-cinit at schottelius.org)
+ *    2007 Nico Schottelius (nico-cinit at schottelius.org)
  *
  *    part of cLinux/cinit
  *
- *    Pre calculate the service tree
+ *    This function reads the full service tree and generates
+ *    a list of services to begin shutdown (those that are not
+ *    wanted or needed by any other service).
  */
 
-#include <stdlib.h>
-#include "cinit.h"
-#include "svc.h"
+#include <stdio.h>         /* NULL        */
+#include "svc.h"           /* structs     */
+#include "cinit.h"         /* structs     */
 
-struct listitem *gen_svc_tree(char *svc)
+struct dep *gen_halt_list(struct listitem *svc_tree)
 {
-   struct listitem   *li;
-   struct dep        *deps;
+   struct dep        *list = NULL, *new = NULL;
+   struct listitem   *tmp;
 
-   /* only do something if the service is not already known */
-   if((li=list_search(svc)))           return li;
-
-   /* create a template, so other instances won't try to recreate us */
-   if(!(li=svc_create(svc)))           return NULL;
-
-   if(!check_add_deps(li,DEP_NEEDS))   return NULL;
-   if(!check_add_deps(li,DEP_WANTS))   return NULL;
-
-   /* no dependencies? then you are a start service */
-   if(!li->wants && !li->needs) {
-      deps = dep_create(li);
-      if(!deps) return NULL;
-      dep_entry_add(&svc_init,deps);
-
-      /* Mark it as being in the startup list, so it does not
-       * get added again in a dep_needs_wants_add call */
-      li->status |= ST_IN_LIST;
+   /* no tree? nothing to shutdown. */
+   if(!svc_tree) {
+      return NULL;
    }
 
-   return li;
+   /* find all services that do not depend on other services:
+    * - empty wants? (currently ignore them)
+    * - empty needs!
+    */
+   tmp = svc_tree;
+   do {
+      if(tmp->needs == NULL) {
+         /* create new dependency */
+         new = dep_create(tmp);
+         if(!new) return NULL;
+
+         dep_entry_add(&list,new);
+      }
+      tmp = tmp->next;
+   } while(tmp != svc_tree);
+   
+   return list;
 }
