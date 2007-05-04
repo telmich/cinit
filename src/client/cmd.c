12837ce048028fdf16dcd1d16d97ba0c74b7710b
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-05-04 22:27:47 +0200

    Initial template for cmd

---
+++ src/client/cmd.c
@@ -0,0 +1,16 @@
+/***********************************************************************
+ *
+ *    2007 Nico Schottelius (nico-cinit at schottelius.org)
+ *
+ *    part of cLinux/cinit
+ *
+ *    cmd - the cinit client program - sends commands to cinit
+ */
+
+
+/* Arguments:
+
+int main(int argc, char **argv)
+{
+
+}
