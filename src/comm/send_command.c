1f204434b9de0ce85258583aa299be2f1422abed
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-05-04 22:02:59 +0200

    Added template of send_command

---
+++ src/comm/send_command.c
@@ -0,0 +1,14 @@
+/***********************************************************************
+ *
+ *    2007 Nico Schottelius (nico-cinit at schottelius.org)
+ *
+ *    part of cLinux/cinit
+ *
+ *    A client sends a command to cinit
+ */
+
+#include "comm.h"       /* message struct definition */
+
+send_command(struct s_cmd)
+{
+}
