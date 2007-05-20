a1cdc774c4bb57491dea7984cc97a13e95632078
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-05-20 10:42:33 +0200

    Use wrapper for signals

--- src/client/halt.kill.c
+++ src/client/halt.kill.c
@@ -7,16 +7,16 @@
  *    Halt the system through a kill call
  */
 
-#include <sys/types.h>     /* kill()         */
-#include <signal.h>        /* kill()         */
+#include <sys/types.h>     /* kill()            */
+#include <signal.h>        /* kill()            */
 
-#include "messages.h"      /* MSG_*          */
-#include "cinit.h"         /* print_errno()  */
-
+#include "messages.h"      /* MSG_*             */
+#include "cinit.h"         /* print_errno()     */
+#include "signals.h"       /* signals mapping   */
 
 int main()
 {
-   if(kill(1,SIGUSR1) == -1) {
+   if(kill(1,SIG_CINIT_HALT) == -1) {
       print_errno(MSG_HALT_KILL);
       return 1;
    }
