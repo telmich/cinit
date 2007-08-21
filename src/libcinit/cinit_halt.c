6c3c63d01dcb74cd763b8aed004d1344576f125c
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-08-21 20:27:26 +0200

    Add idea of cinit library

---
+++ src/libcinit/cinit_halt.c
@@ -0,0 +1,24 @@
+/***********************************************************************
+ *
+ *    2007 Nico Schottelius (nico-cinit at schottelius.org)
+ *
+ *    part of cLinux/cinit
+ *
+ *    cinit_halt(): call for library
+ */
+
+
+#include ""       /* include global header */
+int cinit_halt(int how)
+{
+   switch(how) {
+      case 'p':   /* power off */
+      break;
+
+      default:
+         break;
+      }
+   }
+
+   return 1;
+}
