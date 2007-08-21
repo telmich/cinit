5c083f1c27f47d55037ce97d7631e5c2f150bc7b
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-08-21 20:12:26 +0200

    Add header for cmd process

---
+++ src/include/cmd.h
@@ -0,0 +1,26 @@
+/***********************************************************************
+ *
+ *    2007 Nico Schottelius (nico-cinit //@\\ schottelius.org)
+ *
+ *    part of cLinux/cinit
+ *
+ *    Header file for the cmd (cinit management)
+ */
+
+#ifndef CINIT_CMD_HEADER
+#define CINIT_CMD_HEADER
+
+#define CMD_USAGE "cmd - cinit management\n\n"  \
+   "Usage:\n\ncmd -[ednwphr]\n"                 \
+   "   -e: enable service\n"                    \
+   "        including dependencies\n"           \
+   "   -d: disable service\n"                   \
+   "        including dependencies\n"           \
+   "   -n: without needs\n"                     \
+   "        Skip wants when en/disabling\n"     \
+   "   -w: without wants\n"                     \
+   "        Skip needs when en/disabling\n"     \
+   "   -h: halt\n"                              \
+   "   -p: poweroff\n"                          \
+   "   -r: reboot\n"
+#endif
