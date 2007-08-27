830d8b09a9652ebe4c4623ccef7574ecde67f1ce
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-08-27 23:22:05 +0200

    Add initial version of cinit_send_to.c

---
+++ src/libcinit/cinit_send_to.c
@@ -0,0 +1,17 @@
+/***********************************************************************
+ *
+ *    2007 Nico Schottelius (nico-cinit at schottelius.org)
+ *
+ *    part of cLinux/cinit
+ *
+ *    sends data from the client to cinit
+ */
+
+
+#include ""       /* include global header */
+
+/* gets a bunch of bytes and returns the answer from cinit */
+char *cinit_send_to(char *data, int len)
+{
+   return 1;
+}
