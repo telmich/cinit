660db410268910c769572146d4ac04907e3f8f83
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-08-28 18:58:39 +0200

    Finish cinit_ipc_csend.c

---
+++ src/ipc/msgq/cinit_ipc_csend.c
@@ -0,0 +1,33 @@
+/***********************************************************************
+ *
+ *    2007 Nico Schottelius (nico-cinit schottelius.org)
+ *
+ *    part of cLinux/cinit
+ *
+ *    Client sends messages to cinit
+ *
+ */
+
+#include <sys/ipc.h>             /* ftok              */
+#include <sys/msg.h>             /* msgget            */
+
+#include <string.h>              /* memcpy()          */
+
+#include "config.h"
+#include "intern.h"              /* print_errno       */
+#include "msgq.h"
+
+int cinit_ipc_csend(struct cinit_question *data)
+{
+   struct msgq_client msg;
+
+   /* copy structure into the msgq-structure */
+   memcpy(&(msg.msg),data, sizeof(msg.msg));
+
+   if(msgsnd(mq_out,&msg,sizeof(msg),0) < 0) {
+      print_errno("msgsnd,csend");
+      return 0;
+   }
+
+   return 1;
+}
