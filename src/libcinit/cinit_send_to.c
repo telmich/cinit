1faaf7d0de7d8b5a1a53f865eef5856a878f794a
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-08-28 20:02:32 +0200

    Cleanup cinit_send_to

--- src/libcinit/cinit_send_to.c
+++ src/libcinit/cinit_send_to.c
@@ -11,16 +11,11 @@
 #include "ipc.h"        /* IPC                     */
 #include "cinit.h"      /* struct cinit_message    */
 
-//#include <stdio.h>      /* NULL           0         */
-
-/* gets a bunch of bytes and returns the answer from cinit */
 int cinit_send_to(struct cinit_question *data, struct cinit_answer *res)
 {
    if(!cinit_ipc_logon())        return 0;
    if(!cinit_ipc_csend(data))    return 0;
    if(!cinit_ipc_cread(res))     return 0;
    if(!cinit_ipc_logoff())       return 0;
-
-//   if(!cinit_ipc_connect())      return 0; /* unneeded? */
    return 1;
 }
