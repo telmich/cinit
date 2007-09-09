49ad682618756dfe9133115e9045801773b2e204
Nico Schottelius <nico@denkbrett.intern.schottelius.org>
2007-09-09 23:11:00 +0200

    Add also sources for getpid

---
+++ src/comm/answer_svc_pid.c
@@ -0,0 +1,28 @@
+/***********************************************************************
+ *
+ *    2007 Nico Schottelius (nico-cinit at schottelius.org)
+ *
+ *    part of cLinux/cinit
+ *
+ *    return pid of service
+ *
+ */
+
+#include <string.h>     /* strncpy                    */
+#include "svc.h"        /* VERSION                    */
+#include "cinit.h"      /* structure: cinit_answer    */
+
+int answer_svc_pid(char *svc, struct cinit_answer *asr)
+{
+   struct listitem *tmp;
+
+   tmp = list_search(svc);
+   if(!tmp) {
+      asr->ret = CINIT_MSG_SVC_UNKNOWN;
+   } else {
+      asr->ret       = CINIT_MSG_OK;
+      asr->options   = tmp->pid;
+   }
+
+   return 1;
+}
---
+++ src/libcinit/cinit_svc_get_pid.c
@@ -0,0 +1,29 @@
+/***********************************************************************
+ *
+ *    2007 Nico Schottelius (nico-cinit at schottelius.org)
+ *
+ *    part of cLinux/cinit
+ *
+ *    Retrieves status of a service
+ */
+
+
+#include <string.h>     /* str*                 */
+#include "cinit.h"      /* header for clients   */
+
+/* returns either the status (>0)
+ * or -1 on memory error
+ */
+pid_t cinit_svc_get_pid(char *name)
+{
+   struct cinit_question qsn;
+   struct cinit_answer   asr;
+
+   qsn.cmd = CINIT_MSG_GET_PID;
+   strcpy((qsn.data), name);
+   qsn.options = 0;
+
+   if(!cinit_send_to(&qsn, &asr)) return -1;
+
+   return asr.ret;
+}
