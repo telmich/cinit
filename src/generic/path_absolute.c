bf85de91cdcc6195e0def305296fe3567f59e514
Nico Schottelius <nico@denkbrett.schottelius.org>
2008-02-15 21:23:50 +0100

    add gpl3 header

--- src/generic/path_absolute.c
+++ src/generic/path_absolute.c
@@ -1,10 +1,25 @@
-/***********************************************************************
+/*******************************************************************************
  *
- *    2006-2007 Nico Schottelius (nico-cinit at schottelius.org)
+ * 2006-2008 Nico Schottelius (nico-cinit at schottelius.org)
  *
- *    part of cLinux/cinit
+ * This file is part of cinit.
+
+ * cinit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cinit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cinit.  If not, see <http://www.gnu.org/licenses/>.
+
  *
  *    create an absulte path
+ *
  */
 
 #include <unistd.h>                 /* chdir(),getcwd       */
@@ -26,7 +41,7 @@ int path_absolute(char *relpath, char *newpath, size_t size)
       return 0;
    }
 
-   if(!getcwd(newpath,size)) {
+   if(!getcwd(newpath, size)) {
       print_errno(relpath);
       return 0;
    }
