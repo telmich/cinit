5bbad6c2ef0ac3a08f0665a07d998d0c36819326
Nico Schottelius <nico@denkbrett.schottelius.org>
2008-02-15 23:28:04 +0100

    gpl3 headers

--- src/include/cmd.h
+++ src/include/cmd.h
@@ -1,8 +1,22 @@
-/***********************************************************************
+/*******************************************************************************
  *
- *    2007 Nico Schottelius (nico-cinit //@\\ schottelius.org)
+ * 2007-2008 Nico Schottelius (nico-cinit at schottelius.org)
  *
- *    part of cLinux/cinit
+ * This file is part of cinit.
+
+ * cinit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cinit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cinit.  If not, see <http://www.gnu.org/licenses/>.
+
  *
  *    Header file for the cmd (cinit management)
  */
@@ -11,15 +25,14 @@
 #define CINIT_CMD_HEADER
 
 #define CMD_USAGE "cmd - cinit management\n\n"  \
-   "cmd -[ed] -[nw] -[ps] [service] -[v]\n\n"   \
+   "cmd -[ed (nw)] -[ps] [service] -[hvV]\n\n"   \
    "   -e: enable service\n"                    \
-   "        including dependencies\n"           \
+   "       n: without needs\n"                  \
+   "           Skip wants when en/disabling\n"  \
+   "       w: without wants\n"                  \
+   "           Skip needs when en/disabling\n\n"\
    "   -d: disable service\n"                   \
-   "        including dependencies\n\n"         \
-   "      -n: without needs\n"                     \
-   "           Skip wants when en/disabling\n"     \
-   "      -w: without wants\n"                     \
-   "           Skip needs when en/disabling\n\n"   \
+   "        see above\n"                        \
    "   -h: get help\n"                          \
    "        this messages"                      \
    "   -p: get process id (pid)\n"              \
@@ -32,7 +45,7 @@
    "        of cmd\n"                           \
    ""
 
-#define CMD_OPTIONS "e:d:hp:s:vV"
+#define CMD_OPTIONS "e::d::hp:s:vV"
 
 #define MSG_KILL     "kill()"
 #define CMD_VERSION  "0.2"
