40feea5569d031860af4b2bd6e975534e76f5dbe
Nico Schottelius <nico@denkbrett.schottelius.org>
2008-02-16 00:54:50 +0100

    add cp_data.c

---
+++ src/comm/cp_data.c
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ *
+ * 2008      Nico Schottelius (nico-cinit at schottelius.org)
+ *
+ * This file is part of cinit.
+
+ * cinit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cinit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cinit.  If not, see <http://www.gnu.org/licenses/>.
+
+ *
+ *    fill the data field of qsn and asn
+ *
+ */
+
+#include <string.h>     /* strncpy                    */
+#include "cinit.h"      /* CINIT_DATA_LEN             */
+
+void cp_data(char data[], char *src)
+{
+   strncpy(data, src, CINIT_DATA_LEN);
+}
