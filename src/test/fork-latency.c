6b0ceaaa3e51494223671bc6ec828c29d1605d4d
Nico Schottelius <nico@ikn.schottelius.org>
2009-09-01 20:00:36 +0200

    do NOT use WNOHANG
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

--- src/test/fork-latency.c
+++ src/test/fork-latency.c
@@ -1,8 +1,7 @@
-
 /*************
  *
  * A very small program to show that the child() of a fork()
- * proceess can exit before fork() returns in the parent
+ * proceess can / CANNOT exit before fork() returns in the parent
  *
  * Nico Schottelius, Copying: GPLv3, 20070925
  *
@@ -50,61 +49,70 @@
 #include <sys/wait.h>           /* waitpid */
 #include <stdio.h>              /* printf, NULL */
 
-#define MAX 500                 /* number of forks */
+#define MAX 5000             /* number of forks */
 
 pid_t list[MAX];
-int i = MAX;
+int i;
+int got_sig;
 
-void sig_child(int status)
+void sig_child(int sig)
+{
+   got_sig = 1;
+}
+
+void reap_child()
 {
    int o;
    int found;
+   int status;
    pid_t pid;
 
-   while((pid = waitpid(-1, &status, WNOHANG)) > 0) {
-      if(pid == -1)
-         return;                /* ignore errors */
-
+   /* WNOHANG is evil: it makes us call the loop MORE THAN ONCE
+    * PER DEAD CHILD! -> registering a dead child via signal
+    * handler makes WNOHANG obsolete
+    */
+   //while((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+   while((pid = waitpid(-1, &status, 0)) > 0) {
       found = 0;
       // no need to search the whole list, use o = i instead
       // for(o = 0; o < MAX; o++) {
       for(o = i; o < MAX; o++) {
          if(list[o] == pid) {
+            list[o] = 0;  /* empty */
             found = 1;
             break;
          }
       }
       if(found) {
-         printf("(%d) found\n", i);
+         printf("(%d) found\n", pid);
       } else {
-         printf("(%d) child exited before fork() returned\n", i);
+         printf("(%d) Unknown child exited\n", pid);
       }
    }
+
+   got_sig = 0;
 }
 
 int main()
 {
    struct sigaction sa;
 
+   /* listen to SIGCHLD */
    sigemptyset(&sa.sa_mask);
-
    sa.sa_flags = 0;
    sa.sa_handler = sig_child;
    sa.sa_flags = SA_NOCLDSTOP;
    sigaction(SIGCHLD, &sa, NULL);
-
-   for(i = MAX - 1; i >= 0; i--)
-      list[i] = 0;
 
    i = MAX;
 
    for(i = MAX - 1; i >= 0; i--) {
       list[i] = fork();
 
-      /*
-       * child exists immediately 
-       */
-      if(list[i] == 0)
-         return 0;
+      /* child exists immediately */
+      if(list[i] == 0) return 0;
+      
+      /* parent code */
+      if(got_sig) reap_child();
    }
 }
