ef174c54990d9a9dc7ca7ae5cce19f545cb7d0fa
Nico Schottelius <nico@ikn.schottelius.org>
2009-09-02 20:41:15 +0200

    mark status that are in the translation table
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

--- src/include/svc.h
+++ src/include/svc.h
@@ -31,18 +31,18 @@
  */
 enum cinit_svc_status {
    /* first define basics */
-   CINIT_ST_SH_ONCE     = 0x1,     /* service SHould be started once            */
-   CINIT_ST_SH_RESPAWN  = 0x2,     /* service SHould respawn                    */
+   CINIT_ST_SH_ONCE     = 0x1,     /* service SHould be started once           x*/
+   CINIT_ST_SH_RESPAWN  = 0x2,     /* service SHould respawn                   x*/
    CINIT_ST_ONCE_OK     = 0x4,     /* service was successfully started once     */
    CINIT_ST_ONCE_FAIL   = 0x8,     /* service failed to start                   */
    CINIT_ST_RESPAWNING  = 0x10,    /* service is respawning                     */
-   CINIT_ST_NEED_FAILD  = 0x20,    /* this service is not started, need failed  */
+   CINIT_ST_NEED_FAILD  = 0x20,    /* this service is not started, need failed x*/
    CINIT_ST_IN_LIST     = 0x40,    /* this service is being started (= in list) */
    CINIT_ST_BAD_ERR     = 0x80,    /* some kind of error that SHOULD NOT happen */
-   CINIT_ST_ONCE_RUN    = 0x100,   /* the once process is currently running     */
+   CINIT_ST_ONCE_RUN    = 0x100,   /* the once process is currently running    x*/
    CINIT_ST_NOT_EXIST   = 0x200,   /* there's no such service in our database   */
-   CINIT_ST_STOPPING    = 0x400,   /* service was running, is being stopped     */
-   CINIT_ST_STOPPED     = 0x800,   /* service is stopped                        */
+   CINIT_ST_STOPPING    = 0x400,   /* service was running, is being stopped    x*/
+   CINIT_ST_STOPPED     = 0x800,   /* service is stopped                       x*/
    CINIT_ST_STOP_FAIL   = 0x1000   /* service failed to stop..strange situation */
 };
 
