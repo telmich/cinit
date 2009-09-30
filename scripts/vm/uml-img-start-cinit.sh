810f6981b8be375e750ac9b3289a54842e279c1a
Nico Schottelius <nico@ikn.schottelius.org>
2009-09-30 07:49:15 +0200

    create clean script to start cinit from an linux image via uml
    Signed-off-by: Nico Schottelius <nico@ikn.schottelius.org>

---
+++ scripts/vm/uml-img-start-cinit.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# 
+# 2009      Nico Schottelius (nico-cinit at schottelius.org)
+# 
+# This file is part of cinit.
+#
+# cinit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# cinit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with cinit. If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Start an image with cinit as init
+#
+
+ROOT="$1"; shift
+
+linux "ubd0=$ROOT" init=/sbin/cinit "$@"
--- scripts/vm/uml-start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-ROOT=$(dirname $0)/Debian-3.0r0.ext2
-
-linux ubd0=$ROOT init=/sbin/cinit "$@"
